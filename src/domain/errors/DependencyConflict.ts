12f0d74edcd506cd1196b926378b56f842bb3130
Felipe Marinho <github@felipemarinho.dev>
2023-12-14 21:00:46 -0800

    create the custom error `DependencyConflict`

---
+++ src/domain/errors/DependencyConflict.ts
@@ -0,0 +1,16 @@
+import { SerializableStatic } from "../types.js";
+import { DomainError } from "./DomainError.js";
+
+export class DependencyConflictError extends DomainError {
+  constructor (parentClass: SerializableStatic, dependency: SerializableStatic, locator: any) {
+    super(`${parentClass.name} with locator 
+        ${JSON.stringify(locator)} cannot be removed because 
+        ${dependency.name} has dependencies to it.`,
+      parentClass,  
+      {
+        code: 'DEPENDENCY_LOCK',
+        status: 403
+      }
+    )
+  }
+}
