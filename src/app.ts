1f59d4f2f35294b32bf79f8eb69673c2a2593867
Felipe Marinho <github@felipemarinho.dev>
2023-12-18 12:25:30 -0800

    create initialize file

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,66 @@
-export default async function main(envs: Record<string, string | undefined>) { 
-  console.log(envs.PORT);
+import { type AppConfig, appConfig } from "./config.js";
+import { ClassRepository } from "./data/ClassRepository.js";
+import { ParentRepository } from "./data/ParentRepository.js";
+import { StudentRepository } from "./data/StudentRepository.js";
+import { TeacherRepository } from "./data/TeacherRepository.js";
+import { WebLayer } from "./presentation/index.js";
+import { ClassService } from "./services/ClassService.js";
+import { ParentService } from "./services/ParentService.js";
+import { StudentService } from "./services/StudentService.js";
+import { TeacherService } from "./services/TeacherService.js";
+
+export type Application = (
+  config: AppConfig,
+  services: any
+) => Promise<{
+  start: () => Promise<void>,
+  stop: () => Promise<void>,
+}>
+
+export type ServiceList = ReturnType<typeof initDependencies>['services']; 
+
+function initDependencies() {
+  const repositories = {
+    class: new ClassRepository(),
+    student: new StudentRepository(),
+    parent: new ParentRepository(),
+    teacher: new TeacherRepository(),
+  }
+
+  const parentService = new ParentService(repositories.parent);
+  const studentService = new StudentService(repositories.student, parentService);
+  const teacherService = new TeacherService(repositories.teacher);
+  const classService = new ClassService(repositories.class, teacherService, studentService);
+
+  return {
+    services: {
+      class: classService,
+      parent: parentService,
+      student: studentService,
+      teacher: teacherService,
+    }
+  }
 }
-await main(process.env)
+
+async function main(layers: Application, config: AppConfig) {
+  const {services} = initDependencies();
+  const { start, stop } = await layers(config, services);
+
+  process.on('SIGINT', async  () => {
+    console.info('SIGINT signal received')
+    await stop();
+  })
+  process.on('SIGTERM', async  () => {
+    console.info('SIGTERM signal received')
+    await stop();
+  })
+  process.on('unhandleRejection', (reason) => {
+    console.info('unhandleRejection signal received', reason)
+  })
+  process.on('uncaughtException', async  (error) => {
+    console.info('uncaughtException signal received', error)
+    await stop();
+  })
+  return await start()
+}
+await main(WebLayer, appConfig)
